4f4c564fc8b9c93a0ed67857b02325b6544c015a
AnyaaForger <anyaa.forg.aaa@gmail.com>
2023-08-24 20:12:34 +0700

    Change message to messages and add log messages

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/webhook", (req,res)=>{
     let challenge = req.query["hub.challenge"];
     let token = req.query["hub.verify_token"];
 
-    console.log(challenge);
+    // console.log(challenge);
     
     if(mode && token){
         if(mode === "subscribe" && token === VERIFY_TOKEN){
@@ -32,15 +32,22 @@ app.get("/webhook", (req,res)=>{
 
 app.post("/webhook", (req, res)=>{
     let body_param = req.body;
+
     console.log(JSON.stringify(body_param, null, 2));
+
     if(body_param.object){
+        console.log("Inside body param...");
         if(body_param.entry && 
             body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.message && 
-            body_param.entry[0].changes[0].value.message[0]){
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]){
                 let phone_no_id = req.body.entry[0].changes[0].value.metadata.phone_no_id;
-                let from = req.body.entry[0].changes[0].value.message[0].from;
-                let msg_body = req.body.entry[0].changes[0].value.message[0].text.body;
+                let from = req.body.entry[0].changes[0].value.messages[0].from;
+                let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+
+                console.log("Phone number: " + phone_no_id);
+                console.log("From: " + from);
+                console.log("Body param: " + msg_body);
 
                 axios({
                     method:"POST",
